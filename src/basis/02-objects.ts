5d19c0e8249b4cc7d671e1a4108feec3a256cec8
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-16 23:20:02 -0600

    + do array types

--- src/basis/02-objects.ts
+++ src/basis/02-objects.ts
@@ -1,7 +1,5 @@
 // export const pokemonsIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// export const pokemonsNames = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie'];
-
 // pokemonsIds.push('Bulbasaur'); // Error: Argument of type '"Bulbasaur"' is not assignable to parameter of type 'number'.
 
 interface Pokemon {
@@ -18,4 +16,11 @@ export const bulbasaur: Pokemon = {
 export const ivysaur: Pokemon = {
     id: 2,
     name: "Ivysaur",
+    hp: 60,
 }
+
+export const pokemons: Pokemon[] = [];
+
+pokemons.push(bulbasaur, ivysaur);
+
+console.log(pokemons);

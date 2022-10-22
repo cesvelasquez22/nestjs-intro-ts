eb361c4d88f8df101d17e47d70648e3ad206ecaf
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-21 21:23:25 -0600

    + do classes and async methods axios

--- src/basis/03-classes.ts
+++ src/basis/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { PokeResponse } from '../types/poke.types';
+
 // export class Pokemon {
 //     id: number;
 //     name: string;
@@ -19,7 +22,15 @@
 // }
 
 export class Pokemon {
-  constructor(public id: number, public name: string, public hp?: number) {}
+  get imgUrl() {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.id}.png`;
+  }
+
+  constructor(
+    public readonly id: number,
+    public name: string,
+    public hp?: number
+  ) {}
 
   attack() {
     console.log(`${this.name} attacked!`);
@@ -27,6 +38,17 @@ export class Pokemon {
 
   defend() {
     console.log(`${this.name} defended!`);
+  }
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  async getMoves() {
+    const { data } = await axios.get<PokeResponse>(
+      `https://pokeapi.co/api/v2/pokemon/${this.id}`
+    );
+    return data.moves;
   }
 }
 
@@ -36,4 +58,8 @@ export const ivysaur = new Pokemon(2, "Ivysaur", 60);
 bulbasaur.attack();
 ivysaur.defend();
 
-console.log(bulbasaur, ivysaur);
+console.log(bulbasaur);
+bulbasaur.scream();
+console.log(ivysaur);
+
+ivysaur.getMoves().then((moves) => console.log(moves));
